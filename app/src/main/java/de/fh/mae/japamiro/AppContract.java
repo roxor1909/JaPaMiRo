ce0e5aa9e85d7e0357a588f1ec6d6a088967ad93
roxor1909 <robin.doennebrink001@stud.fh-dortmund.de>
2017-07-06 11:24:32 +0200

    create WHEATER TABLE IN DB

--- app/src/main/java/de/fh/mae/japamiro/AppContract.java
+++ app/src/main/java/de/fh/mae/japamiro/AppContract.java
@@ -12,7 +12,6 @@ public final class AppContract {
     }
 
     public static class ProfilEntry implements BaseColumns {
-        //ToDo profil mit selectedWIndrichtung boolean usw
         public static final String TABLE_NAME = "profile";
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_WINDRICHTUNG = "windrichtung";
@@ -26,5 +25,18 @@ public final class AppContract {
         public static final String COLUMN_NAME_STATION = "station";
         public static final String COLUMN_NAME_MIN_TEMP = "min_temperatur";
         public static final String COLUMN_NAME_SELECTED_MIN_TEMP = "selected_min_temp";
+    }
+
+    public static class WeatherEntry implements BaseColumns {
+        public static final String TABLE_NAME = "wetterdaten";
+        public static final String COLUMN_NAME_METER_PRO_SEKUNDE = "Metter_pro_Sekude";
+        public static final String COLUMN_NAME_K_M_H = "k_m_h";
+        public static final String COLUMN_NAME_KNOTEN = "Knoten";
+        public static final String COLUMN_NAME_BFT = "BFT";
+        public static final String COLUMN_NAME_WINDRICHTUNG = "Windrichtung";
+        public static final String COLUMN_NAME_TEMPERATUR = "Temperatur";
+        public static final String COLUMN_NAME_HUM = "Hum";
+        public static final String COLUMN_NAME_ZEITPUNKT = "Zeitpunkt";
+
     }
 }
